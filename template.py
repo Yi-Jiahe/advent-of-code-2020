04535a3a452cfccabc9d671d667eefbb84d428a4
Jiahe <yi.jiahe00@gmail.com>
2021-12-05 23:41:25 +0800

    Update template

--- template.py
+++ template.py
@@ -1,15 +1,20 @@
 from aoc_logging import logger
 
+title = ""
 
 def part_one():
     print("--- Part One ---")
+    logger.info(title)
+    logger.info("--- Part One ---")
 
 
 def part_two():
     print("--- Part Two ---")
+    logger.info(title)
+    logger.info("--- Part Two ---")
 
 
 if __name__ == '__main__':
-    print("")
+    print(title)
     part_one()
     part_two()
